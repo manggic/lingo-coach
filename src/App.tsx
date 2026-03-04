66518cc503b2b3d1be94f07116dc45c08e0e255f
manish <manishmahto198@gmail.com>
2026-03-04 13:20:34 +0530

    updated model

--- src/App.tsx
+++ src/App.tsx
@@ -135,7 +135,7 @@ const App = () => {
 
     const callAI = async () => {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${apiKey}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
